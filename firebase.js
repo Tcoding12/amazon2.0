7a43eaefff52f7344d80166a055a657e36ed9dfa
Tino <anthony1@gmail.com>
2021-05-22 15:13:04 -0500

    update

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAqckEgdgi0euSlkne8n8BLGVhxH675ATI",
+    authDomain: "y-58883.firebaseapp.com",
+    projectId: "y-58883",
+    storageBucket: "y-58883.appspot.com",
+    messagingSenderId: "309790219414",
+    appId: "1:309790219414:web:2ab4ef5125d6c2d176b6b7"
+};
